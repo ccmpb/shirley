55e768eac8d001f31d3f61f860a798ce1d94ef3a
Colin Campbell <colin@elusivelabs.com>
2020-05-06 21:48:08 -0400

    get random word

--- src/app.js
+++ src/app.js
@@ -1,7 +1,20 @@
 const express = require('express')
+const { exec } = require('child_process');
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Shirly, this is a super groovy name generator.'))
+app.get('/', (req, res) => {
+	exec('shuf -n1 data/words', (err, stdout, stderr) => {
+		if (err) {
+			return res.send(err)
+		}
+		if (stderr) {
+			return res.send(stderr)
+		}
+		console.log(stdout)
+
+		res.send(stdout.toLowerCase())
+	})
+})
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
